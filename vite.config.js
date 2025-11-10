31ddddf4e16d8cec82b8116e7258a669c645f418
Nick Garay <nicolasgaray@icloud.com>
2025-11-10 18:15:55 -0500

    Enable configuration of endpoint and title and add documentation to env files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -11,46 +11,55 @@ const cesiumSource = "node_modules/cesium/Build/Cesium";
 const cesiumBaseUrl = "public/cesium";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: JSON.stringify(process.env.VITE_VIEWER_ENDPOINT),
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'osh-js': fileURLToPath(new URL('./lib/osh-js', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['fsevents', 'node:path'],
-    }
-  },
-  ssr: {
-    noExternal: ['osh-js', 'cesium', 'leaflet']
-  },
-  optimizeDeps: {
-    include: [ 'cesium', 'leaflet', 'osh-js'],
-    exclude: []
-  },
-  worker: {
-    format: 'es',
-    rollupOptions: {
 
+
+// The function argument automatically provides 'mode'
+export default defineConfig(({ mode }) => {
+  // Load env file based on 'mode' in the current working directory.
+  // Second argument is the directory where env files are located.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_VIEWER_ENDPOINT,
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      viteStaticCopy({
+        targets: [
+          { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+        ]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'osh-js': fileURLToPath(new URL('./lib/osh-js', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['fsevents', 'node:path'],
+      }
+    },
+    ssr: {
+      noExternal: ['osh-js', 'cesium', 'leaflet']
+    },
+    optimizeDeps: {
+      include: [ 'cesium', 'leaflet', 'osh-js'],
+      exclude: []
+    },
+    worker: {
+      format: 'es',
+      rollupOptions: {
+
+      }
+    },
+    define: {
+      CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
     }
-  },
-  define: {
-    CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
   }
 })
