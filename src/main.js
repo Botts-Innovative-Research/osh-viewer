aa24c2a4c29d3fa5eeeadcc9bc44c4bef92e8d9b
Ian Patterson <14809287+tipatterson-dev@users.noreply.github.com>
2025-11-11 15:21:28 -0600

    update deps, remove TreeView from main.js as it was rolled into the components portion of Vuetify

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import { VTreeview } from 'vuetify/labs/VTreeview';
 import '@mdi/font/css/materialdesignicons.css';
 
 import App from './App.vue';
@@ -19,7 +18,6 @@ import router from './router/index.js';
 const vuetify = createVuetify({
 	components: {
 		...components,
-		VTreeview,
 	},
 	directives,
 	icons: {
