c5f42ac031a9fcc477cd973e8eedf0e050d22cce
Salwa Jeries <annette.jeries@gmail.com>
2026-01-07 13:42:11 -0600

    Fixed datastream part of GeoPTZ component

--- src/components/visualizations/VisualizationWrapper.vue
+++ src/components/visualizations/VisualizationWrapper.vue
@@ -38,7 +38,8 @@ const VisualizationComponent = computed(() => visualizationMap[props.vizType]);
 		></Video>
 		<GeoPTZ
 			:visualization="viz"
-			:datasource="viz.visualizationComponents.dataSource"
+			:datasource="Array.isArray(viz.visualizationComponents.dataSource) ? viz.visualizationComponents.dataSource[0] : viz.visualizationComponents.dataSource"
+			:controlstream="Array.isArray(viz.controlstream) ? viz.controlstream[0] : viz.controlstream"
 			v-if="viz.type === 'geoPtz'"
 		></GeoPTZ>
 		<slot name="after" />
