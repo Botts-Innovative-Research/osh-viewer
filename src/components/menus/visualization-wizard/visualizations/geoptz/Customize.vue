76e2b9c6e668b75041536b9225aac1c8d42a33b0
kalynstricklin <kalynstricklin@botts-inc.com>
2026-01-19 16:11:00 -0600

    update

--- src/components/menus/visualization-wizard/visualizations/geoptz/Customize.vue
+++ src/components/menus/visualization-wizard/visualizations/geoptz/Customize.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import IconControl from '../../viz-components/customizations/IconControl.vue';
-import VideoCustomizationOptions
-  from "@/components/menus/visualization-wizard/visualizations/video/VideoCustomizationOptions.vue";
 </script>
 
 <template>
