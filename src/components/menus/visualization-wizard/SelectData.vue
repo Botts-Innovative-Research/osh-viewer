643464c41dab2b4f2b1368235d25e3bd322d682a
Salwa Jeries <annette.jeries@gmail.com>
2026-03-06 10:28:05 -0600

    Added input validation for select data

--- src/components/menus/visualization-wizard/SelectData.vue
+++ src/components/menus/visualization-wizard/SelectData.vue
@@ -83,34 +83,46 @@ useComponentValidation(valid, emit)
 		v-model="selectedSystems"
 		:items="listSystems"
 		hint="Select one or more systems"
-		label="Select system(s)"
+		label="System(s)*"
 		multiple
 		persistent-hint
 		item-title="name"
 		item-value="id"
     class="mb-4"
+		chips
+		clearable
+		validate-on="blur"
+		:rules="[(v: any) => !!v.length || 'At least one system must be selected']"
 	></v-autocomplete>
 	<!-- Select for datastreams -->
 	<v-autocomplete
 		v-model="selectedDatastreams"
 		:items="listDatastreams"
 		hint="Select one or more datastreams"
-		label="Select datastream(s)"
+		label="Datastream(s)*"
 		multiple
 		persistent-hint
 		item-title="name"
 		:item-value="(item: OSHDatastream) => item"
     class="mb-4"
+		chips
+		clearable
+		validate-on="blur"
+		:rules="[(v: any) => !!v.length || 'At least one datastream must be selected']"
 	></v-autocomplete>
   <!-- Select for controlstreams -->
   <v-autocomplete
       v-model="selectedControlstreams"
       :items="listControlstreams"
       hint="Select one or more controlstreams"
-      label="Select controlstream(s)"
+      :label="'Controlstream(s)' + (props.requireCs ? '*' : '')"
       multiple
       persistent-hint
       item-title="name"
       :item-value="(item: OSHControlStream) => item"
+			chips
+			clearable
+			validate-on="blur"
+			:rules="props.requireCs? [(v: any) => !!v.length || 'At least one controlstream must be selected'] : []"
   ></v-autocomplete>
 </template>
