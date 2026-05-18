6f8c415850dc0f67a33bc976aeeb78af18c62a8c
Salwa Jeries <annette.jeries@gmail.com>
2026-05-18 14:52:07 -0500

    Added input validation for node form

--- src/components/menus/NodeConfigForm.vue
+++ src/components/menus/NodeConfigForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useOSHConnectStore } from '@/stores/oshconnectstore.js';
 import { useUIStore } from '@/stores/uistore';
 import { OSHNode } from '@/lib/OSHConnectDataStructs';
@@ -8,7 +8,7 @@ import { showToast } from '@/composables/useToast';
 const oshconnect = useOSHConnectStore().getInstance();
 const uiStore = useUIStore();
 
-// Define reactive variables for the form fields
+// Form fields
 const nodeName = ref('Test');
 const nodeHost = ref(window.location.hostname);
 const nodePort = ref('8080');
@@ -16,6 +16,19 @@ const nodePath = ref('sensorhub/api');
 const nodeUser = ref('admin');
 const nodePassword = ref('admin');
 const tls = ref(false);
+
+const isValid = computed(() => {
+	if (
+		nodeName.value &&
+		nodeHost.value &&
+		nodePort.value &&
+		nodePath.value &&
+		nodeUser.value &&
+		nodePassword.value
+	)
+		return true;
+	else return false;
+});
 
 async function createNode() {
 	// This function will be called when the button is clicked
@@ -62,33 +75,50 @@ function sanitizeAPIRoot(path) {
 					label="Node Name"
 					v-model="nodeName"
 					placeholder="Test"
+					class="mb-2"
+					:rules="[(v) => !!v || 'Name is required']"
 					required
 				/>
 				<v-text-field
 					label="Node Host"
 					v-model="nodeHost"
 					placeholder="localhost"
+					:rules="[(v) => !!v || 'Host is required']"
 					required
 				/>
 				<v-text-field
 					label="Node Port"
 					v-model="nodePort"
 					placeholder="8181"
+					type="number"
+					inputmode="numeric"
+					onkeydown="if(['e', 'E', '+', '-'].includes(event.key)) event.preventDefault();"
+					class="mb-2"
+					:rules="[(v) => !!v || 'Port is required']"
 					required
 				/>
 				<v-text-field
 					label="Node Path"
 					v-model="nodePath"
 					placeholder="sensorhub/api"
+					class="mb-2"
+					:rules="[(v) => !!v || 'Path is required']"
+					required
 				/>
 				<v-text-field
 					label="Node User"
 					v-model="nodeUser"
+					class="mb-2"
+					:rules="[(v) => !!v || 'Username is required']"
+					required
 				/>
 				<v-text-field
 					label="Node Password"
 					v-model="nodePassword"
 					type="password"
+					class="mb-2"
+					:rules="[(v) => !!v || 'Password is required']"
+					required
 				/>
 				<v-checkbox
 					label="TLS: Secure"
@@ -101,6 +131,7 @@ function sanitizeAPIRoot(path) {
 						type="submit"
 						color="success"
 						variant="tonal"
+						:disabled="!isValid"
 						>Create Node</v-btn
 					>
 					<v-btn
