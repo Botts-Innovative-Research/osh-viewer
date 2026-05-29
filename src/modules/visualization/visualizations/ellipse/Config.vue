a59187dd0ad74ff6676b58e67aa3e28795fb116e
Salwa Jeries <annette.jeries@gmail.com>
2026-05-29 15:56:37 -0500

    Added getColor and getIconColor to visualizations

--- src/modules/visualization/visualizations/ellipse/Config.vue
+++ src/modules/visualization/visualizations/ellipse/Config.vue
@@ -44,6 +44,16 @@ const checkedRoles = reactive({
 			}
 		},
 	}),
+	color: computed({
+		get: () => vizwizStore.dsConfig.color?.selected ?? false,
+		set: (val: boolean) => {
+			if (val) {
+				vizwizStore.updateDsConfig('color', { selected: val });
+			} else {
+				delete vizwizStore.dsConfig.color;
+			}
+		},
+	}),
 });
 
 // Initialize dsConfig with position, semiMajorAxis, and semiMinorAxis selected by default when mounted
@@ -82,13 +92,17 @@ const rolePositionValid = ref<boolean>(false);
 const roleSemiMajorAxisValid = ref<boolean>(false);
 const roleSemiMinorAxisValid = ref<boolean>(false);
 const roleEllipseIdValid = ref<boolean>(false);
+const roleColorValid = ref<boolean>(false);
 const valid = computed(() => {
 	// If role is checked, must be valid. If not checked, ignore validity
 	const positionValid = checkedRoles.position ? rolePositionValid.value : true;
 	const semiMajorAxisValid = checkedRoles.semiMajorAxis ? roleSemiMajorAxisValid.value : true;
 	const semiMinorAxisValid = checkedRoles.semiMinorAxis ? roleSemiMinorAxisValid.value : true;
 	const ellipseIdValid = checkedRoles.ellipseId ? roleEllipseIdValid.value : true;
-	return positionValid && semiMajorAxisValid && semiMinorAxisValid && ellipseIdValid;
+	const colorValid = checkedRoles.color ? roleColorValid.value : true;
+	return (
+		positionValid && semiMajorAxisValid && semiMinorAxisValid && ellipseIdValid && colorValid
+	);
 });
 useComponentValidation(valid, emit);
 </script>
@@ -146,6 +160,20 @@ useComponentValidation(valid, emit);
 			role="ellipseId"
 			multiple
 			v-model:valid="roleEllipseIdValid"
+		/>
+	</v-container>
+
+	<!-- Color -->
+	<v-container>
+		<v-checkbox
+			label="Color"
+			v-model="checkedRoles.color"
+		></v-checkbox>
+		<DataSourcePicker
+			v-if="checkedRoles.color"
+			role="color"
+			multiple
+			v-model:valid="roleColorValid"
 		/>
 	</v-container>
 </template>
