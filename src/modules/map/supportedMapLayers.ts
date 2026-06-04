2c02df5a2d856b67d378a7b7cd2ebc47d8a64045
Salwa Jeries <annette.jeries@gmail.com>
2026-06-04 00:19:41 -0500

    Added supported map layers type

---
+++ src/modules/map/supportedMapLayers.ts
@@ -0,0 +1,29 @@
+import { useSettingsStore } from '@/stores/settingsstore';
+import {
+	VisualizationRegistry,
+	VisualizationType,
+} from '../visualization/registry/VisualizationRegistry';
+import PointMarkerLayer from 'osh-js/source/core/ui/layer/PointMarkerLayer';
+import LoBLayer from 'osh-js/source/core/ui/layer/viewer/LoB.js';
+import EllipseLayer from 'osh-js/source/core/ui/layer/EllipseLayer';
+import PolylineLayer from 'osh-js/source/core/ui/layer/PolylineLayer';
+
+export const SupportedMapLayers = {
+	PointMarkerLayer: PointMarkerLayer,
+	LoBLayer: LoBLayer,
+	EllipseLayer: EllipseLayer,
+	PolylineLayer: PolylineLayer,
+} as const;
+export type SupportedMapLayer = (typeof SupportedMapLayers)[keyof typeof SupportedMapLayers];
+
+/**
+ * Determine if a visualization type is compatible with the currently focused map
+ * @param type
+ * @returns
+ */
+export function isMapLayerCompatible(type: VisualizationType) {
+	const settingsStore = useSettingsStore();
+	const descriptor = VisualizationRegistry[type];
+	if (!descriptor) return false;
+	return descriptor.supportedMaps?.includes(settingsStore.focusedMap);
+}
