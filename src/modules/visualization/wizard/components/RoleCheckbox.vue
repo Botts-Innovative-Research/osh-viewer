cfd4622f10528332b7392ffc9b2709dc838a4a3e
Salwa Jeries <annette.jeries@gmail.com>
2026-06-05 15:24:23 -0500

    Added info tooltip and combined role checkbox components

---
+++ src/modules/visualization/wizard/components/RoleCheckbox.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import InfoTooltip from '@/components/ui/InfoTooltip.vue';
+
+const props = defineProps<{
+	label: string; // Label for checkbox
+	disabled?: boolean; // Checkbox disabled status
+	tooltip: string; // Text content for info tooltip
+}>();
+const model = defineModel<boolean>();
+</script>
+<template>
+	<v-row class="align-center h-auto">
+		<v-col cols="auto">
+			<v-checkbox
+				v-model="model"
+				:label="props.label"
+				:disabled="props.disabled ?? false"
+				hide-details
+			/>
+		</v-col>
+		<v-col
+			cols="auto"
+			class="tooltip-padding"
+		>
+			<info-tooltip :content="tooltip" />
+		</v-col>
+	</v-row>
+</template>
+<style scoped>
+.tooltip-padding {
+	padding: 0;
+	padding-bottom: 2px;
+}
+</style>
