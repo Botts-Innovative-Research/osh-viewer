d08133c7a3ee9f7b4959bbcd3c1b137633292f13
kalynstricklin <kalynstricklin@botts-inc.com>
2026-06-18 09:46:38 -0500

    Update MissionBuilder.vue

--- src/modules/visualization/visualizations/mission/MissionBuilder.vue
+++ src/modules/visualization/visualizations/mission/MissionBuilder.vue
@@ -5,7 +5,7 @@ import { computed, onBeforeUnmount, ref, watch } from 'vue';
 import { randomUUID } from 'osh-js/source/core/utils/Utils.js';
 import { useMapStore } from '@/stores/mapstore';
 import { showToast } from '@/composables/useToast';
-import SweApi from 'osh-js/source/core/datasource/sweapi/SweApi.datasource.js';
+import ConSysApi from 'osh-js/source/core/datasource/consysapi/ConSysApi.datasource.js';
 import MissionCommandPad from './MissionCommandPad.vue';
 import {
 	createDatasource,
@@ -94,9 +94,9 @@ const fileInputRef = ref<any | null>(null);
 const selectedFile = ref<File | null>(null);
 const exportFilename = ref<string>('mission');
 
-const droneDatasourceLLA = ref<SweApi | null>(null);
-const droneHomeDatasource = ref<SweApi | null>(null);
-let dsInstances = ref<SweApi[]>([]);
+const droneDatasourceLLA = ref<ConSysApi | null>(null);
+const droneHomeDatasource = ref<ConSysApi | null>(null);
+let dsInstances = ref<ConSysApi[]>([]);
 
 let homeLocation = ref<{ lat: number; lon: number; alt: number }>({ lat: 0, lon: 0, alt: 0 });
 
@@ -505,7 +505,7 @@ function generateMissionControlPlan() {
 	};
 }
 
-function onLLAListener(dsInstance: SweApi) {
+function onLLAListener(dsInstance: ConSysApi) {
 	const dataBroadcastChannel = new BroadcastChannel(DATASOURCE_DATA_TOPIC + dsInstance.id);
 
 	dataBroadcastChannel.onmessage = (message) => {
