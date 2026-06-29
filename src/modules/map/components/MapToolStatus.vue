24ac56c0b8e946e37b1a200fcb8f4bf1f3eaafa4
Salwa Jeries <annette.jeries@gmail.com>
2026-06-29 17:55:48 -0500

    Moved round pin to waypoints directory, updated map tools status component

--- src/modules/map/components/MapToolStatus.vue
+++ src/modules/map/components/MapToolStatus.vue
@@ -1,74 +1,79 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { useMapStore } from '@/stores/mapstore';
+import { useSettingsStore } from '@/stores/settingsstore';
+import { ICON_BASE, ICON_OPTIONS, iconPathBuilder } from '@/lib/icons';
+import { getColoredIconUrl } from '../services/colorId.service';
 
 const mapStore = useMapStore();
+const settingsStore = useSettingsStore();
+
+const isActive = computed(() => {
+	return !!mapStore.isGeoPTZSelected || !!mapStore.selectedWaypoints;
+});
 
 const isHovered = ref(false);
 </script>
 
 <template>
-	<v-card
-		class="pa-2 elevation-10"
-		:style="{
-			position: 'absolute',
-			zIndex: 2000,
-			marginTop: '1%',
-			marginLeft: '1%',
-		}"
-		@mouseenter="isHovered = true"
-		@mouseleave="isHovered = false"
-	>
-		<v-card-text class="d-flex flex-column ga-2">
-			<!-- GeoPTZ -->
-			<div class="d-flex align-center ga-2">
+	<v-fade-transition>
+		<v-card
+			class="ma-0 elevation-10"
+			:style="{
+				position: 'absolute',
+				zIndex: 2000,
+				marginTop: '1%',
+				marginLeft: '1%',
+			}"
+			@mouseenter="isHovered = true"
+			@mouseleave="isHovered = false"
+			v-show="isActive"
+		>
+			<v-card-text class="d-flex flex-column ga-2 pa-2">
+				<!-- GeoPTZ -->
 				<div
-					class="status-dot"
-					:class="mapStore.isGeoPTZSelected ? 'bg-green' : 'bg-grey'"
-				/>
-
-				<span>GeoPTZ</span>
-
-				<transition name="fade-slide-x">
-					<span
-						v-if="isHovered"
-						class="ml-1 text-grey"
-					>
-						{{ mapStore.isGeoPTZSelected ? 'selected' : 'not selected' }}
-					</span>
-				</transition>
-			</div>
-
-			<!-- Mission Builder -->
-			<div class="d-flex align-center ga-2">
+					class="d-flex align-center ga-2"
+					v-show="mapStore.isGeoPTZSelected"
+				>
+					<v-icon
+						icon="mdi-crosshairs-gps"
+						color="green"
+					/>
+					<span>GeoPTZ</span>
+					<transition name="fade-slide-x">
+						<span
+							v-if="isHovered"
+							class="ml-1 text-grey"
+						>
+							cursor active
+						</span>
+					</transition>
+				</div>
+				<!-- Mission Builder -->
 				<div
-					class="status-dot"
-					:class="mapStore.selectedWaypoints ? 'bg-green' : 'bg-grey'"
-				/>
-
-				<span>Mission Builder</span>
-
-				<transition name="fade-slide-x">
-					<span
-						v-if="isHovered"
-						class="ml-1 text-grey"
-					>
-						{{ mapStore.selectedWaypoints ? 'selected' : 'not selected' }}
-					</span>
-				</transition>
-			</div>
-		</v-card-text>
-	</v-card>
+					class="d-flex align-center ga-2"
+					v-show="!!mapStore.selectedWaypoints"
+				>
+					<v-icon
+						icon="mdi-crosshairs-gps"
+						color="green"
+					/>
+					<span>Mission Builder</span>
+					<transition name="fade-slide-x">
+						<span
+							v-if="isHovered"
+							class="ml-1 text-grey"
+						>
+							cursor active
+						</span>
+					</transition>
+				</div>
+			</v-card-text>
+		</v-card>
+	</v-fade-transition>
 </template>
 
 <style scoped>
-.status-dot {
-	width: 10px;
-	height: 10px;
-	border-radius: 50%;
-	flex-shrink: 0;
-}
-
 .fade-slide-x-enter-active,
 .fade-slide-x-leave-active {
 	transition: all 0.2s ease;
