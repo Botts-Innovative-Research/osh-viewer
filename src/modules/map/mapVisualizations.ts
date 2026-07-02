ec2c721e094455a19f4733c7793c6e6cd3d4a9e1
Salwa Jeries <annette.jeries@gmail.com>
2026-07-02 13:12:28 -0500

    Updated getOrientation to check for yaw or just property

--- src/modules/map/mapVisualizations.ts
+++ src/modules/map/mapVisualizations.ts
@@ -82,7 +82,10 @@ export async function createPointMarkerLayer(
 				dataSourceIds: [dsInstance.id],
 				handler: (rec: any) => {
 					return {
-						heading: rec[dsProps.properties.orientation.property].heading,
+						heading:
+							rec[dsProps.properties.orientation.property].heading ??
+							rec[dsProps.properties.orientation.property].yaw ??
+							rec[dsProps.properties.orientation.property],
 					};
 				},
 			};
