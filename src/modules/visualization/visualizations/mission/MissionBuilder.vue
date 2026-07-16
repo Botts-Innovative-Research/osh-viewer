5d3ff73d68b61c7ed08dabf5f026e5da3369350c
kalynstricklin <kalynstricklin@botts-inc.com>
2026-07-16 14:50:05 -0500

    send all missions button

--- src/modules/visualization/visualizations/mission/MissionBuilder.vue
+++ src/modules/visualization/visualizations/mission/MissionBuilder.vue
@@ -174,6 +174,9 @@ watch(
 	{ immediate: true }
 );
 
+async function sendAllMissions() {
+  console.log('send all missions')
+}
 onBeforeUnmount(() => {
 	cleanupDatasources();
 });
@@ -306,6 +309,16 @@ const hasCommandPad = computed(
 				</v-window-item>
 			</v-window>
 		</v-sheet>
+
+		<v-btn
+			v-if="validVisualizations.length > 1"
+			block
+			class="mt-4"
+			color="primary"
+			variant="tonal"
+			@click="sendAllMissions"
+		>
+			Send All Missions
 	</v-container>
 </template>
 
