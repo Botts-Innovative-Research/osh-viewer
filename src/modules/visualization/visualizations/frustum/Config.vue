c142674c03c54a1131fe6158e1ad3f4bc2973e6e
Salwa Jeries <annette.jeries@gmail.com>
2026-07-17 18:01:55 -0400

    Added automap to frustum

--- src/modules/visualization/visualizations/frustum/Config.vue
+++ src/modules/visualization/visualizations/frustum/Config.vue
@@ -17,7 +17,10 @@ const props = withDefaults(
 	}
 );
 
-const { checkedRoles, validRoles, valid, include } = useConfig(props.configRoles, !props.optional);
+const { checkedRoles, validRoles, valid, include, autoMap } = useConfig(
+	props.configRoles,
+	!props.optional
+);
 
 // Validation
 const emit = defineEmits<VisualizationComponentEmits>();
@@ -40,6 +43,16 @@ useComponentValidation(effectiveValid, emit);
 
 	<v-expand-transition>
 		<div v-if="include">
+			<v-container class="pb-0">
+				<v-btn
+					variant="tonal"
+					color="primary"
+					prepend-icon="mdi-auto-fix"
+					@click="autoMap"
+				>
+					Auto Map Streams
+				</v-btn>
+			</v-container>
 			<v-container v-for="config in props.configRoles">
 				<RoleCheckbox
 					v-model="checkedRoles[config.role]"
