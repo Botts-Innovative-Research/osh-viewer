cac5bc71cf85de927d027ba43a1456c6c97b2a9c
Salwa Jeries <annette.jeries@gmail.com>
2026-08-17 12:31:09 -0500

    Finished basic UI for inputting new map

--- src/modules/settings/map-settings/OfflineMapSettings.vue
+++ src/modules/settings/map-settings/OfflineMapSettings.vue
@@ -1,14 +1,89 @@
 <script setup lang="ts">
 import DeleteButton from '@/components/ui/DeleteButton.vue';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useSettingsStore } from '@/stores/settingsstore';
 import { useMapStore } from '@/stores/mapstore';
+import { showToast } from '@/composables/useToast';
+import { randomUUID } from 'osh-js/source/core/utils/Utils.js';
+
+// Stores
 const settingsStore = useSettingsStore();
 const mapStore = useMapStore();
 const enableOfflineMaps = computed({
 	get: () => settingsStore.enableOfflineMaps ?? false,
 	set: (val) => settingsStore.setEnableOfflineMaps(val),
 });
+
+// Form fields
+const fileServerUrl = ref('http://localhost:8080');
+const mapName = ref('New Map');
+const mapPath = ref('');
+const minZoom = ref(12);
+const maxZoom = ref(20);
+const lat = ref(0);
+const lon = ref(0);
+const hasBuildings = ref(false);
+const buildingsPath = ref('');
+
+// Form actions and status
+const isFormOpen = ref(false);
+const isValid = computed(() => {
+	if (
+		fileServerUrl.value &&
+		mapName.value &&
+		mapPath.value &&
+		minZoom.value !== null &&
+		maxZoom.value !== null &&
+		lat.value !== null &&
+		lat.value >= -90 &&
+		lat.value <= 90 &&
+		lon.value !== null &&
+		lon.value >= -180 &&
+		lon.value <= 180 &&
+		(!hasBuildings.value || (hasBuildings.value && buildingsPath.value))
+	)
+		return true;
+	else return false;
+});
+
+async function addMap() {
+	mapStore.addOfflineMapLayer({
+		id: randomUUID(),
+		fileServerUrl: fileServerUrl.value,
+		mapName: mapName.value,
+		mapPath: mapPath.value,
+		minZoom: minZoom.value,
+		maxZoom: maxZoom.value,
+		lat: lat.value,
+		lon: lon.value,
+		hasBuildings: hasBuildings.value,
+		buildingsPath: buildingsPath.value,
+	});
+
+	showToast('Map added successfully', 'SUCCESS');
+	isFormOpen.value = false;
+	resetForm();
+}
+function resetForm() {
+	fileServerUrl.value = 'http://localhost:8080';
+	mapName.value = 'New Map';
+	mapPath.value = '';
+	minZoom.value = 12;
+	maxZoom.value = 20;
+	lat.value = 0;
+	lon.value = 0;
+	hasBuildings.value = false;
+	buildingsPath.value = '';
+}
+const cancelForm = () => {
+	isFormOpen.value = false;
+	resetForm();
+};
+const onKeyDown = (event: KeyboardEvent) => {
+	if (['e', 'E'].includes(event.key)) {
+		event.preventDefault();
+	}
+};
 </script>
 <template>
 	<v-card style="padding: 0px">
@@ -30,140 +105,200 @@ const enableOfflineMaps = computed({
 					</template>
 				</v-list-item>
 				<v-list-item>
-					<v-btn
-						block
-						variant="tonal"
-						color="primary"
-						prepend-icon="mdi-plus-circle"
-						>Add Offline Map</v-btn
-					>
-					<v-btn
-						block
-						variant="outlined"
-						color="default"
-						prepend-icon="mdi-close"
-						>Cancel</v-btn
-					>
+					<v-fade-transition v-if="!isFormOpen">
+						<v-btn
+							@click="isFormOpen = true"
+							block
+							variant="tonal"
+							color="primary"
+							prepend-icon="mdi-plus-circle"
+							>Add Offline Map</v-btn
+						>
+					</v-fade-transition>
+					<v-fade-transition v-else>
+						<v-btn
+							@click="cancelForm"
+							block
+							variant="outlined"
+							color="default"
+							prepend-icon="mdi-close"
+							>Cancel</v-btn
+						>
+					</v-fade-transition>
 				</v-list-item>
-				<v-list-item>
-					<!--			<v-form @submit.prevent="createNode()">-->
-					<!--				<v-text-field-->
-					<!--					label="Node Name"-->
-					<!--					v-model="nodeName"-->
-					<!--					placeholder="Test"-->
-					<!--					:rules="[(v) => !!v || 'Name is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-text-field-->
-					<!--					label="Node Host"-->
-					<!--					v-model="nodeHost"-->
-					<!--					placeholder="localhost"-->
-					<!--					:rules="[(v) => !!v || 'Host is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-number-input-->
-					<!--					label="Node Port"-->
-					<!--					v-model="nodePort"-->
-					<!--					placeholder="8080"-->
-					<!--					variant="outlined"-->
-					<!--					inputmode="numeric"-->
-					<!--					onkeydown="if(['e', 'E', '+', '-'].includes(event.key)) event.preventDefault();"-->
-					<!--					:rules="[(v) => !!v || 'Port is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-text-field-->
-					<!--					label="Node Path"-->
-					<!--					v-model="nodePath"-->
-					<!--					placeholder="sensorhub/api"-->
-					<!--					:rules="[(v) => !!v || 'Path is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-text-field-->
-					<!--					label="Node User"-->
-					<!--					v-model="nodeUser"-->
-					<!--					:rules="[(v) => !!v || 'Username is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-text-field-->
-					<!--					label="Node Password"-->
-					<!--					v-model="nodePassword"-->
-					<!--					type="password"-->
-					<!--					:rules="[(v) => !!v || 'Password is required']"-->
-					<!--					required-->
-					<!--				/>-->
-					<!--				<v-checkbox-->
-					<!--					label="TLS: Secure"-->
-					<!--					v-model="tls"-->
-					<!--					color="primary"-->
-					<!--					hide-details-->
-					<!--				></v-checkbox>-->
+				<v-expand-transition>
+					<v-list-item v-if="isFormOpen">
+						<v-form
+							@submit.prevent="addMap()"
+							class="d-flex flex-column pt-4 ga-2"
+						>
+							<v-text-field
+								label="File Server Path"
+								v-model="fileServerUrl"
+								placeholder="http://localhost:8080"
+								:rules="[(v) => !!v || 'File Server Path is required']"
+								required
+							/>
+							<v-text-field
+								label="Map Name"
+								v-model="mapName"
+								placeholder="New Map"
+								:rules="[(v) => !!v || 'Map Name is required']"
+								required
+							/>
+							<v-text-field
+								label="Map Path"
+								v-model="mapPath"
+								placeholder="maps/field"
+								:rules="[(v) => !!v || 'Map Path is required']"
+								required
+							/>
+							<v-row>
+								<v-col cols="6">
+									<v-number-input
+										label="Min Zoom"
+										v-model="minZoom"
+										placeholder="12"
+										variant="outlined"
+										inputmode="numeric"
+										@onkeydown="onKeyDown"
+										:rules="[(v) => !!v || 'Min Zoom is required']"
+										required
+									/>
+								</v-col>
+								<v-col cols="6">
+									<v-number-input
+										label="Max Zoom"
+										v-model="maxZoom"
+										placeholder="20"
+										variant="outlined"
+										inputmode="numeric"
+										@onkeydown="onKeyDown"
+										:rules="[(v) => !!v || 'Max Zoom is required']"
+										required
+									/>
+								</v-col>
+							</v-row>
+							<v-row>
+								<v-col cols="6">
+									<v-text-field
+										label="Center Latitude"
+										v-model="lat"
+										placeholder="0.0"
+										hint="-90 to 90"
+										min="-90"
+										max="90"
+										variant="outlined"
+										type="number"
+										@onkeydown="onKeyDown"
+										hide-spin-buttons
+										:rules="[
+											(v) =>
+												(v !== null && v !== undefined) ||
+												'Latitude is required',
+											(v) => (v >= -90 && v <= 90) || 'Must be -90 to 90',
+										]"
+										required
+									/>
+								</v-col>
+								<v-col cols="6"
+									><v-text-field
+										label="Center Longitude"
+										v-model="lon"
+										placeholder="0.0"
+										hint="-180 to 180"
+										min="-180"
+										max="180"
+										variant="outlined"
+										type="number"
+										@onkeydown="onKeyDown"
+										hide-spin-buttons
+										:rules="[
+											(v) =>
+												(v !== null && v !== undefined) ||
+												'Longitude is required',
+											(v) => (v >= -180 && v <= 180) || 'Must be -180 to 180',
+										]"
+										required
+								/></v-col>
+							</v-row>
 
-					<!--				&lt;!&ndash; Buttons inside the form &ndash;&gt;-->
-					<!--				<v-card-actions>-->
-					<!--					<v-btn-->
-					<!--						text-->
-					<!--						@click="cancelForm"-->
-					<!--						>Cancel</v-btn-->
-					<!--					>-->
-					<!--					<v-btn-->
-					<!--						type="submit"-->
-					<!--						color="primary"-->
-					<!--						variant="tonal"-->
-					<!--						:disabled="!isValid"-->
-					<!--						>Create Node</v-btn-->
-					<!--					>-->
-					<!--				</v-card-actions>-->
-					<!--			</v-form>-->
-				</v-list-item>
-				<v-divider></v-divider>
+							<v-switch
+								label="Has Buildings"
+								v-model="hasBuildings"
+								color="primary"
+								inset="material"
+								hide-details
+							></v-switch>
+							<v-expand-transition>
+								<v-text-field
+									v-if="hasBuildings"
+									label="Buildings Path"
+									v-model="buildingsPath"
+									placeholder=""
+									:rules="[(v) => !!v || 'Buildings Path is required']"
+									required
+								/>
+							</v-expand-transition>
+
+							<!-- Buttons inside the form -->
+							<v-card-actions>
+								<v-btn
+									type="submit"
+									color="primary"
+									variant="tonal"
+									:disabled="!isValid"
+									>Add Offline Map</v-btn
+								>
+							</v-card-actions>
+						</v-form>
+					</v-list-item>
+				</v-expand-transition>
 				<v-list-item>
-					<!--					<v-expansion-panels-->
-					<!--						variant="accordion"-->
-					<!--						rounded="lg"-->
-					<!--						flat-->
-					<!--					>-->
-					<!--						<v-expansion-panel>-->
-					<!--							<v-expansion-panel-title>-->
-					<!--								Current Offline Map Layers-->
-					<!--								<v-fade-transition>-->
-					<!--									<v-badge-->
-					<!--										v-if="mapStore.cesiumMapLayers.length"-->
-					<!--										inline-->
-					<!--										location="top right"-->
-					<!--										:content="mapStore.cesiumMapLayers.length"-->
-					<!--										class="pl-2"-->
-					<!--									/>-->
-					<!--								</v-fade-transition>-->
-					<!--							</v-expansion-panel-title>-->
-					<!--							<v-expansion-panel-text-->
-					<!--								v-if="mapStore.cesiumMapLayers.length"-->
-					<!--								class="layer-list"-->
-					<!--							>-->
-					<!--								<v-list-->
-					<!--									activatable-->
-					<!--									bg-color="transparent"-->
-					<!--								>-->
-					<!--									<v-list-item-->
-					<!--										v-for="layer in mapStore.cesiumMapLayers"-->
-					<!--										:key="layer.id"-->
-					<!--										class="ga-2 px-2"-->
-					<!--									>-->
-					<!--										<template #prepend>-->
-					<!--											<DeleteButton-->
-					<!--												label="Remove"-->
-					<!--												@delete="mapStore.removeLayer(layer.id)"-->
-					<!--											>-->
-					<!--											</DeleteButton>-->
-					<!--										</template>-->
-					<!--										<v-list-item-title>{{ layer.url }}</v-list-item-title>-->
-					<!--									</v-list-item>-->
-					<!--								</v-list>-->
-					<!--							</v-expansion-panel-text>-->
-					<!--							<v-expansion-panel-text v-else>-->
-					<!--								No layers added yet.-->
-					<!--							</v-expansion-panel-text>-->
-					<!--						</v-expansion-panel>-->
-					<!--					</v-expansion-panels>-->
+					<v-expansion-panels
+						variant="accordion"
+						rounded="lg"
+						flat
+					>
+						<v-expansion-panel>
+							<v-expansion-panel-title>
+								Current Offline Maps
+								<v-fade-transition>
+									<v-badge
+										v-if="mapStore.offlineMapLayers.length"
+										inline
+										location="top right"
+										:content="mapStore.offlineMapLayers.length"
+										class="pl-2"
+									/>
+								</v-fade-transition>
+							</v-expansion-panel-title>
+							<v-expansion-panel-text v-if="mapStore.offlineMapLayers.length">
+								<v-list
+									activatable
+									bg-color="transparent"
+								>
+									<v-list-item
+										v-for="map in mapStore.offlineMapLayers"
+										:key="map.id"
+										class="ga-2 px-2"
+									>
+										<template #prepend>
+											<DeleteButton
+												label="Remove"
+												@delete="mapStore.removeOfflineMapLayer(map.id)"
+											>
+											</DeleteButton>
+										</template>
+										<v-list-item-title>{{ map.mapName }}</v-list-item-title>
+									</v-list-item>
+								</v-list>
+							</v-expansion-panel-text>
+							<v-expansion-panel-text v-else>
+								No offline maps added yet.
+							</v-expansion-panel-text>
+						</v-expansion-panel>
+					</v-expansion-panels>
 				</v-list-item>
 			</v-list>
 		</v-card-text>
--- src/stores/mapstore.ts
+++ src/stores/mapstore.ts
@@ -5,7 +5,7 @@ import { MapLayer } from '@/modules/map/adapters/cesium.adapter';
 import { fetchLayerFromUrl } from '@/modules/map/services/cesiumLayer.service';
 import { OSHVisualization } from '@/lib/OSHConnectDataStructs';
 import { GeoOverlay } from '@/modules/map/geo-overlay/types';
-import { MapPoint } from '@/modules/map/types';
+import { MapPoint, OfflineMapLayer } from '@/modules/map/types';
 
 export const useMapStore = defineStore(
 	'map',
@@ -14,6 +14,9 @@ export const useMapStore = defineStore(
 		const currentLLA: Ref<{ latitude: number; longitude: number; altitude: number } | null> =
 			ref(null); // Currently selected LLA coordinates
 		const tempLLA: Ref<MapPoint | null> = ref(null); // Right-clicked LLA coordinates
+
+		/* OFFLINE MAP LAYERS */
+		const offlineMapLayers: Ref<OfflineMapLayer[]> = ref([]);
 
 		/* CESIUM */
 		const cesiumMapLayers: Ref<MapLayer[]> = ref([]);
@@ -39,7 +42,17 @@ export const useMapStore = defineStore(
 			tempLLA.value = null;
 		}
 
-		// Cesium
+		// Offline Map Layers
+		function addOfflineMapLayer(newLayer: OfflineMapLayer) {
+			offlineMapLayers.value.push(newLayer);
+		}
+		function removeOfflineMapLayer(id: string) {
+			offlineMapLayers.value = offlineMapLayers.value.filter(
+				(layer: OfflineMapLayer) => layer.id !== id
+			);
+		}
+
+		// Cesium Map Layers
 		async function addLayer(url: string) {
 			const newLayer: MapLayer = await fetchLayerFromUrl(url);
 			if (newLayer) cesiumMapLayers.value.push(newLayer);
@@ -52,15 +65,18 @@ export const useMapStore = defineStore(
 			selectedMapItem,
 			currentLLA,
 			tempLLA,
+			offlineMapLayers,
 			cesiumMapLayers,
 			setSelectedMapItem,
 			setCurrentLLA,
 			clearCurrentLLA,
 			setTempLLA,
 			clearTempLLA,
+			addOfflineMapLayer,
+			removeOfflineMapLayer,
 			addLayer,
 			removeLayer,
 		};
 	},
-	{ persist: { pick: ['cesiumMapLayers'] } }
+	{ persist: { pick: ['cesiumMapLayers', 'offlineMapLayers'] } }
 );
