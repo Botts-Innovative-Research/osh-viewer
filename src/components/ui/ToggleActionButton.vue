cf12c668712295e313b20c2421bcf2f3e94e74db
Salwa Jeries <annette.jeries@gmail.com>
2026-08-18 11:41:37 -0500

    Fixed ptz validation to allow 0, created toggle action button to toggle ptz controller

---
+++ src/components/ui/ToggleActionButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ActionButton from '@/components/ui/ActionButton.vue';
+
+const props = withDefaults(
+	defineProps<{
+		toggleOn: boolean;
+		toolName: string;
+	}>(),
+	{}
+);
+</script>
+<template>
+	<v-tooltip
+		location="bottom"
+		:text="`${props.toggleOn ? 'Hide' : 'Show'} ${props.toolName}`"
+	>
+		<template v-slot:activator="{ props: tooltipProps }">
+			<ActionButton
+				@submit="$emit('submit')"
+				:label="props.toolName"
+				:icon="`mdi-${props.toggleOn ? 'eye' : 'eye-off'}`"
+				variant="plain"
+				:color="`${props.toggleOn ? 'primary' : 'grey'}`"
+				v-bind="tooltipProps"
+			/>
+		</template>
+	</v-tooltip>
+</template>
